cfb76b8fade0e74a3e598f1849abb1efe64d61bd
Deneb101 <danidmecha@gmail.com>
2026-02-02 13:56:39 +0100

    Pulled latest changes from github

--- P1/tests/test_first_week.py
+++ P1/tests/test_first_week.py
@@ -5,6 +5,7 @@ EPS-UAM 2026
 """
 
 import sys
+import os
 from django.test import TestCase
 
 from catalog.models import Book, BookInstance, Language, Genre, Author
@@ -13,7 +14,7 @@ class FirstWeekTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         try:
-            from P1.populate_catalog import populate
+            from populate_catalog import populate
             populate()
         except ImportError:
             print('The module populate_catalog does not exist')
