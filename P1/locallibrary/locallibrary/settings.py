f6b7fddb9f151faffe1b3dc75e435dcebe401393
linxl2706 <xinliang2005lin@gmail.com>
2026-02-03 12:20:38 +0100

    Modified DATABASES from setting.py

--- P1/locallibrary/locallibrary/settings.py
+++ P1/locallibrary/locallibrary/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,6 +81,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(default='postgres://alumnodb:alumnodb@localhost:5432/psi',conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
